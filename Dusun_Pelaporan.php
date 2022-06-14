b8a8220df098bd4e57feac7904370607f31fa29f
Desak Putu Sri Wulandari <81306004+desaksw31@users.noreply.github.com>
2022-06-14 13:38:41 +0800

    Update Dusun_Pelaporan.php

--- Dusun_Pelaporan.php
+++ Dusun_Pelaporan.php
@@ -7,7 +7,7 @@
     <title>Formulir Pelaporan(Dusun)</title>
     <link rel="icon" href="image\Badung.png" type="image/icon type">
     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="style3.css">
+    <link rel="stylesheet" type="text/css" href="style4.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
